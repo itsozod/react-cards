b19d3efb3ac85bd8edb5437a7a96d0e4bbb01e7b
Ozod <o.bukhorizoda@mail.ru>
2023-12-06 11:49:10 +0500

    fixed the case where name, age or email or img can be empty so it will return some texts

--- src/userProfile/UserProfile.jsx
+++ src/userProfile/UserProfile.jsx
@@ -3,10 +3,14 @@ import "./UserProfile.css";
 export default function UserProfile({ img, name, age, email, children }) {
   return (
     <div className="card">
-      <img className="profile-img" src={img} alt="profile" />
-      <p className="info">{name}</p>
-      <p className="info">{age}</p>
-      <p className="info">{email}</p>
+      {img ? (
+        <img className="profile-img" src={img} alt="profile" />
+      ) : (
+        "Image not found"
+      )}
+      {name ? <p className="info">{name}</p> : <p>Name not found</p>}
+      {age ? <p className="info">{age}</p> : <p>Age not found</p>}
+      {email ? <p className="info">{email}</p> : <p>Email not found</p>}
       {children}
     </div>
   );
